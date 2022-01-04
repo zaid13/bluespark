fffd04c39a336d185497c15fe4627d705d39a6c5
zaid13 <xaidsaeed@gmail.com>
2022-01-04 21:51:49 +0500

    backend complete

---
+++ lib/providers/CommandProvider.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class CommandProvider with ChangeNotifier {
+  CommandProvider();
+
+  Future<void> _getDeviceType() async {}
+  Future<void> _getMap() async {}
+  Future<void> _getScaller() async {}
+  Future<void> _getWUT() async {}
+  Future<void> _IntializeBlueSparkService() async {}
+
+
+}
